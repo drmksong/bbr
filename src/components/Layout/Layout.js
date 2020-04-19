367cc7d3aabe65b53ad000bde32bd72b0263312a
drmksong <singsong@paran.com>
2020-04-19 21:59:54 +0900

    04/19 home notebook2

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Aux from "../../hoc/Aux";
 import classes from "./Layout.module.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 
 const layout = (props) => {
   return (
     <Aux>
-      <div className={classes.Content}>Toolbar, SideDrawler, Backdrop</div>
+      <Toolbar></Toolbar>
+      {/* <div className={classes.Content}>Toolbar, SideDrawler, Backdrop</div> */}
       <main className={classes.Content}>{props.children}</main>
     </Aux>
   );
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,5 +1,18 @@
 import React from "react";
+import classes from "./Toolbar.module.css";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
 
-const toolbar = (props) => {};
+const toolbar = (props) => {
+  return (
+    <header className={classes.Toolbar}>
+      <div>Menu</div>
+      <Logo></Logo>
+      <nav>
+        <NavigationItems></NavigationItems>
+      </nav>
+    </header>
+  );
+};
 
 export default toolbar;
